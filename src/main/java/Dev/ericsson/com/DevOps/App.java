f114b45a5aea45d53786b1a68b734c657b6b1234
Your Name <you@example.com>
2018-05-11 18:14:45 +0530

    yes

---
+++ src/main/java/Dev/ericsson/com/DevOps/App.java
@@ -0,0 +1,13 @@
+package Dev.ericsson.com.DevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
